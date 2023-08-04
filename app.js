fe70bec6db48c58ee0270ffa938bf7f1482d7dc9
julie-log <wnfl2828@gmail.com>
2023-08-04 14:00:47 +0900

    #3.3 Events (12:38)

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const title = document.querySelector(".hello h1");
 const titles = document.querySelectorAll(".hello h1");
+
+function handleTitleClick() {
+  console.log("h1 is " + titles[0].style.color + "!!!");
+}
+title.style.color = "red";
+titles[0].addEventListener("click", handleTitleClick);
+titles[1].addEventListener("click", handleTitleClick);
+titles[2].addEventListener("click", handleTitleClick);
 console.log(title.innerHTML);
 console.log(titles[0]);
