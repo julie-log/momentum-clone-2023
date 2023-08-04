965b8923bd97dc97dcb480a484f8e59fb9cc2598
julie-log <wnfl2828@gmail.com>
2023-08-04 14:45:48 +0900

    #3.8 CSS in Javascript part Three (07:57)

--- app.js
+++ app.js
@@ -2,11 +2,7 @@ const h1 = document.querySelector(".hello h1");
 
 function handleTitleClick() {
   const clickedClass = "clicked";
-  if (h1.className === clickedClass) {
-    h1.className = "";
-  } else {
-    h1.className = clickedClass;
-  }
+  h1.classList.toggle(clickedClass);
 }
 
 h1.addEventListener("click", handleTitleClick);
