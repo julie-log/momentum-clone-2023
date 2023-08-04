5e8b98d14193ba7a6dfc22dac71ef2067a5df715
julie-log <wnfl2828@gmail.com>
2023-08-04 20:21:26 +0900

    #5.2 PadStart (07:42)

--- js/clock.js
+++ js/clock.js
@@ -3,7 +3,10 @@ clock.innerHTML = "00:00:00";
 
 function getClock() {
   const date = new Date();
-  clock.innerHTML = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerHTML = `${hours}:${minutes}:${seconds}`;
 }
 getClock();
 setInterval(getClock, 1000);
