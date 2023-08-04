808b57a8cc81b1d69d89094b5f368fb41ed0b6b3
julie-log <wnfl2828@gmail.com>
2023-08-04 14:15:08 +0900

    #3.4 Events part Two (08:31)

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const title = document.querySelector(".hello h1");
-const titles = document.querySelectorAll(".hello h1");
 
 function handleTitleClick() {
-  console.log("h1 is " + titles[0].style.color + "!!!");
+  title.style.color = "red";
 }
-title.style.color = "red";
-titles[0].addEventListener("click", handleTitleClick);
-titles[1].addEventListener("click", handleTitleClick);
-titles[2].addEventListener("click", handleTitleClick);
-console.log(title.innerHTML);
-console.log(titles[0]);
+function handleMouseEnter() {
+  title.innerText = "mouse is here !!!";
+}
+function handleMouseLeave() {
+  title.innerText = "mouse is leave !!!";
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
