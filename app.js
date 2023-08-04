35387851209512aa1569ae3025514fa6cc85181c
julie-log <wnfl2828@gmail.com>
2023-08-04 15:27:24 +0900

    #4.3 Events part Two (08:08)

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const link = document.querySelector("a");
 function onLoginSubmit(e) {
   e.preventDefault();
   console.log(e);
 }
-function handleLinkClick(e) {
-  e.preventDefault();
-  console.dir(e);
-}
+
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
