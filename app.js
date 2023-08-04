bf0d9f57687565a4a307e15d5049c0a09d2a0e0e
julie-log <wnfl2828@gmail.com>
2023-08-04 16:06:58 +0900

    #4.6 Loading Username (10:07)

--- app.js
+++ app.js
@@ -2,13 +2,26 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
 function onLoginSubmit(e) {
   e.preventDefault();
   const username = loginInput.value;
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  localStorage.setItem("username", username);
+  localStorage.setItem(USERNAME_KEY, username);
+  printGreetings(username);
+}
+
+function printGreetings(username) {
   greeting.innerText = `Hello ${username}!!!`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+if (savedUsername !== null) {
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  printGreetings(savedUsername);
+} else {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+}
