7cff3cb324800fec8a17f1bc65d53765467319e4
julie-log <wnfl2828@gmail.com>
2023-08-04 14:23:23 +0900

    #3.5 More Events (09:48)

--- app.js
+++ app.js
@@ -9,7 +9,23 @@ function handleMouseEnter() {
 function handleMouseLeave() {
   title.innerText = "mouse is leave !!!";
 }
-
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+function handleWindowCopy() {
+  alert("copied!");
+}
+function handleWindowOffline() {
+  alert("no wifi....");
+}
+function handleWindowOnline() {
+  alert("connected wifi....");
+}
 title.addEventListener("click", handleTitleClick);
 title.addEventListener("mouseenter", handleMouseEnter);
 title.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
