875e26c0c44f3a68c42e30ff6ff117ec1e2709db
julie-log <wnfl2828@gmail.com>
2023-08-06 18:41:00 +0900

    #6.1 Background (08:45)

---
+++ js/background.js
@@ -0,0 +1,15 @@
+const images = [
+  "0.jpeg",
+  "1.jpeg",
+  "2.jpeg",
+  "3.jpeg",
+  "4.jpeg",
+  "5.jpeg",
+  "6.jpeg",
+];
+
+const number = Math.floor(Math.random() * images.length);
+const chosenImage = images[number];
+const bgImage = document.createElement("img");
+bgImage.src = `img/${chosenImage}`;
+document.body.appendChild(bgImage);
