cd2fd1636e20ea407b7c19ae7f0df4d2d146cd56
julie-log <wnfl2828@gmail.com>
2023-08-06 20:05:59 +0900

    #7.1 Adding ToDos (07:00)

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,20 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector("#todo-list");
 
+function paintToDo(newToDo) {
+  //   toDoList.innerHTML = `<li><span>${newToDo}</span></li>`;
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.append(span);
+  span.innerText = newToDo;
+  toDoList.appendChild(li);
+}
 function handleToDoSubmit(e) {
   e.preventDefault();
   localStorage.setItem("toDoList", toDoInput.value);
+  const newToDo = toDoInput.value;
   toDoInput.value = "";
-  console.log(localStorage.getItem("toDoList"));
+  paintToDo(newToDo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
