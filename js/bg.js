932a01ed4bf627b0504a6dc8690b02aa6ce7ea5d
julie-log <wnfl2828@gmail.com>
2023-08-19 21:13:28 +0900

    modify 2

--- js/bg.js
+++ js/bg.js
@@ -1,24 +1,12 @@
 const colors = [
-  "#ef5777",
-  "#575fcf",
-  "#4bcffa",
-  "#34e7e4",
-  "#0be881",
-  "#f53b57",
-  "#3c40c6",
-  "#0fbcf9",
-  "#00d8d6",
-  "#05c46b",
-  "#ffc048",
-  "#ffdd59",
-  "#ff5e57",
-  "#d2dae2",
-  "#485460",
-  "#ffa801",
-  "#ffd32a",
-  "#ff3f34",
-  "#f7cac9",
-  "#92a8d1",
+  "#2BC0E4",
+  "#EAECC6",
+  "#085078",
+  "#85D8CE",
+  "#4776E6",
+  "#8E54E9",
+  "#FF8008",
+  "#FFC837",
 ];
 
 const body = document.querySelector("body");
@@ -31,7 +19,6 @@ function handleBackground() {
   const second = newColor[random];
 
   body.style.background = `linear-gradient(45deg, ${first}, ${second})`;
-  console.log(first, second);
 }
 handleBackground();
 button.addEventListener("click", handleBackground);
